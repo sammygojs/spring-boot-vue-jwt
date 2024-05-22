f766581e1cfaf1e4888ccae098ce46109af1daa7
Sumit <akoliyasumit@hotmail.com>
2024-05-22 18:34:24 +0100

    Protected Component

---
+++ src/components/Protected.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="protected">
+      <h2>Protected Area</h2>
+      <p>{{ content }}</p>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  import authHeader from '../services/auth-header';
+  
+  export default {
+    data() {
+      return {
+        content: ''
+      };
+    },
+    mounted() {
+      axios
+        .get('http://localhost:8080/protected', { headers: authHeader() })
+        .then(response => {
+          this.content = response.data;
+        })
+        .catch(error => {
+          this.content = error.response.data.message;
+        });
+    }
+  };
+  </script>
+  
